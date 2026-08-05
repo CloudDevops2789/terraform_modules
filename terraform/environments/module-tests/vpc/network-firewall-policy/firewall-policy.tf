edf2b3208726f2aca94d513b9c22f41dae7eee5b
Yoganand <yoganand.nandakumar27@gmail.com>
2026-08-05 12:17:55 +0000

    remove duplicate module test fixtures nested under vpc

--- terraform/environments/module-tests/vpc/network-firewall-policy/firewall-policy.tf
+++
@@ -1,8 +0,0 @@
-##################################################################################################
-# Module Under Test: Network Firewall Policy
-##################################################################################################
-module "network_firewall_policy" {
-  source            = "../../../modules/network-firewall-policy"
-  firewall_policies = local.firewall_policies
-  tags              = local.default_tags
-}
--- terraform/environments/module-tests/vpc/network-firewall-policy/rule-groups.tf
+++
@@ -1,9 +0,0 @@
-##################################################################################################
-# Supporting Network Firewall Rule Groups
-##################################################################################################
-module "supporting_rule_groups" {
-  source                = "../../../modules/network-firewall-rule-group"
-  stateful_rule_groups  = local.supporting_stateful_rule_groups
-  stateless_rule_groups = local.supporting_stateless_rule_groups
-  tags                  = local.default_tags
-}
